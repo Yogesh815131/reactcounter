2ca094af8d2f3499f5059ac6cfc6ef9468d2e72f
Yogesh815131 <ky815131@gmail.com>
2024-01-24 06:45:44 +0530

    create counter

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from "react";
+
 
 function App() {
+  const [count, setCount] = useState(0);
   return (
     <>
-      <h1>Hellow</h1>
+      <div className='container my-5'>
+        <div className='card text-center my-5'>
+          <div className='card-body'>
+            <h1>Counter App</h1>
+            <h2 className="my-5">{count}</h2>
+            <botton className="btn btn-success mx-3" onClick={()=> setCount(count + 1)}>Increment</botton>
+            <botton className="btn btn-danger mx-3" onClick={ ()=> setCount(count - 1)} disabled={count === 0}>Decrement</botton>
+            <botton className="btn btn-secondary mx-3" onClick={()=> setCount(0)} disabled={count === 0}>Decrement</botton>
+          </div>
+        </div>
+      </div>
     </>
   );
 }
